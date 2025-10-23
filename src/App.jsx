c525c524c1dc1621fffc5fa8f5d7702536d1eabc
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-23 18:15:27 -0400

    made it appear on a browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const questions = [
+    {
+        questionText: 'What is my favorite color?',
+        answerOptions: [
+            { answerText: 'Yellow', isCorrect: false},
+            { answerText: 'Black', isCorrect: false},
+            { answerText: 'Pink', isCorrect: false},
+            { answerText: 'Blue', isCorrect: true},
+        ],
+    },
+
+    {
+        questionText: 'Without lokking at my name how do I spell it?',
+        answerOptions: [
+            { answerText: 'Sadiya', isCorrect: true},
+            { answerText: 'Sadia', isCorrect: false},
+            { answerText: 'Sadiyah', isCorrect: false},
+            { answerText: 'Sadiyya', isCorrect: false},
+        ],
+
+    },
+
+        {
+        questionText: 'What is my Zodiac sign?',
+        answerOptions: [
+            { answerText: 'Arias', isCorrect: false},
+            { answerText: 'Pisces', isCorrect: true},
+            { answerText: 'Leo', isCorrect: false},
+            { answerText: 'Cancer', isCorrect: false},
+        ],
+
+    },
+]
+    return (
+        <div className="App">
+            <div>
+                <div>Quiz App</div>
+                <div>{questions[0].questionText}</div>
+                {questions[0].answerOptions.map((option, index) =>(
+                    <button>{option.answerText}</button>
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default App
+export default App;
