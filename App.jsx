be8f3f8b41c4eef76871cda3695ee756c263d022
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-23 17:59:03 -0400

    added 3 questions so far

--- App.jsx
+++ App.jsx
@@ -2,7 +2,39 @@ import React from 'react';
 
 function App() {
 
-    {}
+const questions = [
+    {
+        questionsText: 'What is my favorite color?',
+        answerOptions: [
+            { answerText: 'Yellow', isCorrect: false},
+            { answerText: 'Black', isCorrect: false},
+            { answerText: 'Pink', isCorrect: false},
+            { answerText: 'Blue', isCorrect: true},
+        ],
+    },
+
+    {
+        questionText: 'Without lokking at my name how do I spell it?',
+        answerOptions: [
+            { answerText: 'Sadiya', isCorrect: true},
+            { answerText: 'Sadia', isCorrect: false},
+            { answerText: 'Sadiyah', isCorrect: false},
+            { answerText: 'Sadiyya', isCorrect: false},
+        ],
+
+    },
+
+        {
+        questionText: 'What is my Zodiac sign?',
+        answerOptions: [
+            { answerText: 'Arias', isCorrect: false},
+            { answerText: 'Pisces', isCorrect: true},
+            { answerText: 'Leo', isCorrect: false},
+            { answerText: 'Cancer', isCorrect: false},
+        ],
+
+    },
+]
     return (
         <div className="App">
             <div>
