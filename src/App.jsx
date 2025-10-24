7fc605c632ee97693bb9892ef9a16d6e798b97b8
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-23 20:27:35 -0400

    fixed my css and jsx to make it readable/understandable

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,50 @@ import React from 'react';
 import './App.css';
 
 function App() {
-
-const questions = [
-    {
-        questionText: 'What is my favorite color?',
-        answerOptions: [
-            { answerText: 'Yellow', isCorrect: false},
-            { answerText: 'Black', isCorrect: false},
-            { answerText: 'Pink', isCorrect: false},
-            { answerText: 'Blue', isCorrect: true},
-        ],
-    },
-
-    {
-        questionText: 'Without lokking at my name how do I spell it?',
-        answerOptions: [
-            { answerText: 'Sadiya', isCorrect: true},
-            { answerText: 'Sadia', isCorrect: false},
-            { answerText: 'Sadiyah', isCorrect: false},
-            { answerText: 'Sadiyya', isCorrect: false},
-        ],
-
-    },
+    const questions = [
+        {
+            questionText: 'What is my favorite color?',
+            answerOptions: [
+                {answerText: 'Yellow', isCorrect: false},
+                {answerText: 'Black', isCorrect: false},
+                {answerText: 'Pink', isCorrect: false},
+                {answerText: 'Blue', isCorrect: true},
+            ],
+        },
 
         {
-        questionText: 'What is my Zodiac sign?',
-        answerOptions: [
-            { answerText: 'Arias', isCorrect: false},
-            { answerText: 'Pisces', isCorrect: true},
-            { answerText: 'Leo', isCorrect: false},
-            { answerText: 'Cancer', isCorrect: false},
-        ],
+            questionText: 'Without looking at my name, how is it spelled?',
+            answerOptions: [
+                {answerText: 'Sadiya', isCorrect: true},
+                {answerText: 'Sadia', isCorrect: false},
+                {answerText: 'Sadiyah', isCorrect: false},
+                {answerText: 'Sadiyya', isCorrect: false},
+            ],
+        },
 
-    },
-]
+        {
+            questionText: 'What is my Zodiac sign?',
+            answerOptions: [
+                {answerText: 'Arias', isCorrect: false},
+                {answerText: 'Pisces', isCorrect: true},
+                {answerText: 'Leo', isCorrect: false},
+                {answerText: 'Cancer', isCorrect: true},
+            ],
+        },
+    ];
+
     return (
         <div className="app-container">
             <div className="quiz-box">
                 <h1>About Me Quiz App</h1>
-            </div>
-            <div>
-                <p>{questions[0].questionText}</p>
-                {questions[0].answerOptions.map((option, index) => (
-                    <button key={index} className="quiz-button">
-                        {option.answerText}
-                    </button>
-                ))}
+                <p className="question-text">{questions[0].questionText}</p>
+                <div className="answer-section">
+                    {questions[0].answerOptions.map((option, index) => (
+                        <button key={index} className="quiz-button">
+                            {option.answerText}
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
     )
